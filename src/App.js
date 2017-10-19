f8a07ca137be37842ce20ca907d2757aa832ed31
Spencer Caldwell <spencer.caldwell@gmail.com>
2017-10-19 12:11:38 -0700

    finished

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Welcome to T-Mobile</h1>
+        <h2>We're excited to meet you</h2>
+        <div className="box">
+          <span className="box-text">400 x 200</span>
+        </div>
       </div>
     );
   }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {configure, shallow} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 import App from './App';
+
+configure({ adapter: new Adapter() });
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
+});
+
+it('render all three JSX components', () => {
+  const wrapper = shallow(<App />);
+  const welcome = <h1>Welcome to T-Mobile</h1>;
+  const excited = <h2>We're excited to meet you</h2>;
+  const div =
+    <div className="box">
+      <span className="box-text">400 x 200</span>
+    </div>;
+    
+  expect(wrapper.contains(welcome)).toEqual(true);
+  expect(wrapper.contains(excited)).toEqual(true);
+  expect(wrapper.contains(div)).toEqual(true);
 });
